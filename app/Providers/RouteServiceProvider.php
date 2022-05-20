6c3246d0f422a049ebe7922ae750df7031e7b542
Antonio Ribeiro <antonio@area17.com>
2022-05-20 11:56:51 +0200

    Lower down PHPStan to 8 because of mixed in Laravel and disable Blast checks

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,17 +23,15 @@ class RouteServiceProvider extends ServiceProvider
      * The controller namespace for the application.
      *
      * When present, controller route declarations will automatically be prefixed with this namespace.
-     *
-     * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    // protected string|null $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->configureRateLimiting();
 
